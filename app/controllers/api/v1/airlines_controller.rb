9f9af379b9987b42fcc80fd3423740919798a696
Hemant-soni-vst-au4 <sonihemant4741@gmail.com>
2021-05-30 21:17:33 +0530

    update method in airline controller

--- app/controllers/api/v1/airlines_controller.rb
+++ app/controllers/api/v1/airlines_controller.rb
@@ -23,6 +23,16 @@ module Api
               end
             end
 
+            def update
+                airline = Airline.find_by(slug: params[:slug])
+  
+                if airline.update
+                  render json: AirlineSerializer.new(airline).serialized_json
+                else
+                  render json: { error: airline.errors.messages }, status: 422
+                end
+            end
+
             def airline_params
                 params.require(:airline).permit(:name, :image_url)
             end
